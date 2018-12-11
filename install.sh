f6e0deb86377c7e500e3cc12006de5db0686af9c
Hayley Song <haejinso@usc.edu>
2018-12-11 15:39:35 -0800

    adds git cloneing commands for tmux and vim plugin managers

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ ln -s Dotfiles/.bash_functions /Users/hayley/.bash_functions
 ln -s Dotfiles/.tmux.conf /Users/hayley/.tmux.conf
 ln -s Dotfiles/.vimrc /Users/hayley/.vimrc
 ln -s Dotfiles/.gitconfig /Users/hayley/.gitconfig
+git clone https://github.com/tmux-plugins/tpm /Users/hayley/.tmux/plugins/tpm
+git clone https://github.com/VundleVim/Vundle.vim.git /Users/hayley/.vim/bundle/Vundle.vim
