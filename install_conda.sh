f51fc9a9a681f7e220d6890e8cdc39642af18d38
Hayley Song <haejinso@usc.edu>
2022-11-02 15:46:59 -0700

    adds cmds to set up base env, ml env, jlab env

--- install_conda.sh
+++ install_conda.sh
@@ -4,3 +4,13 @@ bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
 
+# set up the base environment
+conda install -n base mamba
+
+# set up jupyterlab 3 env
+conda create -n jlab jupyterlab=3 nb_conda_kernel ipywidgets
+
+# set up ML env
+conda create -n ml python=3.7 ipykernel 
+conda install -n ml numpy scipy pandas scikit-learn
+conda install -n ml matplotlib seaborn
